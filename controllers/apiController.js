f9e12f8728b8c5baf832e9a62a3e930c174680de
william dayrit <williamdayritis@gmail.com>
2015-08-22 16:10:31 -0700

    chart showing hardcoded data

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -2,11 +2,16 @@ var User = require('../models/User');
 
 // GET families in JSON
 module.exports.renderFamilies = function(req, res) {
-  User.find({}, function (err, users) {
+  User.find({}, function(err, users) {
     if(err) res.rend(err);
 
     res.json(users);
   });
 };
 
-
+module.exports.renderFamilyChart = function(req, res) {
+  User.find({}, function(err, users) {
+    if(err) res.rend(err);
+    res.render('apitesting/charts', {users: users});
+  });
+};
